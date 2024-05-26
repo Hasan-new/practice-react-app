3e54a5cf882c1032ffc9dd55b68491ade98baee7
unknown <mdhasan.yahoo@gmail.com>
2024-05-26 09:36:24 +0600

    new commit

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -10,7 +10,7 @@ export const router = createBrowserRouter([
       element: <MainLayout/>,
       children:[
         {
-            path: "/",
+            path: '/',
             element: <Home/>,
         },
         {
